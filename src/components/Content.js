f4f0ce87debef95dfd6daad1cfe67cdcd9c0232b
CillianFn <cfennell.stflannans@gmail.com>
2019-01-13 23:23:29 +0000

    routing fix

--- src/components/Content.js
+++ src/components/Content.js
@@ -1,5 +1,5 @@
 import React from 'react'
-import { Switch, Route, BrowserRouter } from 'react-router-dom'
+import { Switch, Route } from 'react-router-dom'
 import Home from './Home'
 import Blog from './Blog'
 import Projects from './Projects'
@@ -8,14 +8,12 @@ import Résumé from './Résumé'
  
 const Content = () =>{
   return(
-    <BrowserRouter basename={process.env.PUBLIC_URL}>
-      <Switch>
-        <Route exact path="/" component={Home}/>
-        <Route path="/blog" component={Blog}/>
-        <Route path="/projects" component={Projects}/>
-        <Route path="/résumé" component={Résumé}/>
-      </Switch>
-    </BrowserRouter>
+    <Switch>
+      <Route exact path="/" component={Home}/>
+      <Route path="/blog" component={Blog}/>
+      <Route path="/projects" component={Projects}/>
+      <Route path="/résumé" component={Résumé}/>
+    </Switch>
   )
 }
  
