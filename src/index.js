3ef490428687e7407470e4d744d2cc9c613fdc5a
CillianFn <cfennell.stflannans@gmail.com>
2019-01-13 23:20:32 +0000

    routing fix

--- src/index.js
+++ src/index.js
@@ -6,4 +6,4 @@ import 'bootstrap-css-only/css/bootstrap.min.css';
 import 'mdbreact/dist/css/mdb.css';
 import App from './App';
  
-ReactDOM.render(<BrowserRouter><App/></BrowserRouter>, document.getElementById('root'));
+ReactDOM.render(<BrowserRouter basename={process.env.PUBLIC_URL}><App/></BrowserRouter>, document.getElementById('root'));
